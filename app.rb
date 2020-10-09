6267fdf5c0590cf36c95bda89035f865a0b5a8d0
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-09 11:48:37 -0700

    Add databases, update requirements for app.rb. Take lunch break.

---
+++ app.rb
@@ -0,0 +1,10 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer_integration')
+require('./lib/volunteer')
+require('pry')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
